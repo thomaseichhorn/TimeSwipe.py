401b834004f10042316024d0f41cfc3fb801b1bd
Ilya Gavrilov <gilyav@gmail.com>
2020-03-28 06:06:50 +0300

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from cmake_setuptools import *
 setuptools.setup(
      name='timeswipe',
-     version='0.0.9',
+     version='0.0.10',
      ext_modules=[CMakeExtension('timeswipe_py')],
      cmdclass={'build_ext': CMakeBuildExt},
      author="Ilya Gavrilov",
